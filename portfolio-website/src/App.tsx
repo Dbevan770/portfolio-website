2953ab1ddf69bc9e80b2b21aa97f776da8c7799e
Daniel Bevan <daniel.j.bevan96@gmail.com>
2023-03-30 16:37:21 +0200

    Designed the basic home section

--- portfolio-website/src/App.tsx
+++ portfolio-website/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Home from "./components/Home/Home";
+import Home from "./scenes/Home/Home";
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <main className="main-content">
+        <Navbar />
+        <Home />
+      </main>
     </div>
   );
 };
